6c2c58bb799779b15807e31c4180007da47d4a7c
Juyoung4 <47167335+Juyoung4@users.noreply.github.com>
2020-07-27 12:54:00 +0900

    두 정수의 합

--- Programmers/LEVEL1/1.py
+++ Programmers/LEVEL1/1.py
@@ -7,3 +7,18 @@ def solution(a, b):
     for i in range(a,b+1):
         answer += i
     return answer
+
+
+
+#다른 사람의 답
+def adder(a, b):
+    # 함수를 완성하세요
+    if a > b: a, b = b, a
+    return sum(range(a,b+1))
+
+
+def adder(a, b):
+    # 함수를 완성하세요
+    return sum(range(min(a,b),max(a,b)+1))
+
+
