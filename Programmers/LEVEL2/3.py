7a81bc2fafb6b451e7a6852149f2d8796cb71cc6
Juyoung4 <vallot7@naver.com>
2020-12-26 19:59:15 +0900

    프로그래머스 기능 개발 update

--- Programmers/LEVEL2/3.py
+++ Programmers/LEVEL2/3.py
@@ -1,1 +1,30 @@
 # 기능 개발
+def solution(progresses, speeds):
+    result, temp = [], 0
+    for i in range(len(progresses)):
+        p = (100-progresses[i])//speeds[i]
+        if (100-progresses[i])%speeds[i] > 0: p += 1
+        if i == 0:
+            temp = p
+            result.append(1)
+            continue
+        if p > temp:
+            result.append(1)
+            temp= p
+        else:
+            result[-1] += 1
+    return result
+        
+# 다른 사람 풀이
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
+    
+print(
+    solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])
+)
