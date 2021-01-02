8eba78d333773033e1de2713d976a100f351c0fe
Juyoung4 <vallot7@naver.com>
2021-01-02 22:35:47 +0900

    no message

--- Inflearn/1일차/loop_question_4.py
+++ Inflearn/1일차/loop_question_4.py
@@ -1,8 +1,11 @@
 # N의 약수 찾기
 def solution(n):
+    count = 0
     for i in range(1,n+1):
         if not n % i:
+            count += 1
             print(i)
+    return count
 
 if __name__ == "__main__":
-    solution(int(input('enter number : ')))
+    print(solution(int(input('enter number : '))))
